42ec861d39ec169474aaca9a447a2d3fc96e692a
Adam <sector-f@users.noreply.github.com>
2018-08-12 23:45:53 -0400

    Added iface info to output

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod domain_state;
 
 use prettytable::Table;
 use virt::connect::*;
+use virt::domain::*;
 use clap::{App, Arg};
 use std::process::exit;
 use domain_state::*;
@@ -19,6 +20,16 @@ struct Domain {
     pub memory: u64,
     pub nr_virt_cpu: u32,
     pub cpu_time: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ifaces: Option<Vec<IfaceStats>>,
+}
+
+#[derive(Serialize)]
+struct IfaceStats {
+    name: String,
+    addresses: Vec<String>,
+    rx_bytes: i64,
+    tx_bytes: i64,
 }
 
 fn main() {
@@ -41,7 +52,7 @@ fn main() {
         .get_matches();
 
     let conn_uri = matches.value_of("connect").expect("Connection URI not found");
-    let conn = match Connect::open_read_only(&conn_uri) {
+    let conn = match Connect::open(&conn_uri) {
         Ok(c) => c,
         Err(_) => exit(1),
     };
@@ -58,6 +69,32 @@ fn main() {
         let state = State::new(s);
         let info = domain.get_info().unwrap();
 
+        let ifaces = match domain.interface_addresses(VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE) {
+            Ok(interfaces) => {
+                let mut iface_stats: Vec<IfaceStats> = Vec::new();
+
+                for interface in interfaces {
+                    let stats: InterfaceStats = domain.interface_stats(&interface.hwaddr).unwrap();
+                    let mut addresses: Vec<String> = Vec::new();
+                    for addr in interface.addrs {
+                        addresses.push(format!("{}/{}", addr.addr, addr.prefix));
+                    }
+                    iface_stats.push(
+                        IfaceStats {
+                            name: interface.name.to_owned(),
+                            addresses: addresses,
+                            rx_bytes: stats.rx_bytes,
+                            tx_bytes: stats.tx_bytes,
+                        }
+                    );
+                }
+                Some(iface_stats)
+            },
+            Err(_) => {
+                None
+            },
+        };
+
         let domain = Domain {
             name: name,
             state: state,
@@ -65,6 +102,7 @@ fn main() {
             max_mem: info.max_mem,
             nr_virt_cpu: info.nr_virt_cpu,
             cpu_time: info.cpu_time,
+            ifaces: ifaces,
         };
 
         list.push(domain);
@@ -76,9 +114,21 @@ fn main() {
         },
         true => {
             let mut table = Table::new();
-            table.add_row(row!["Name", "State", "Memory", "Max Memory", "Virt. CPUs", "CPU Time"]);
+            table.add_row(row!["Name", "State", "Memory", "Max Memory", "Virt. CPUs", "CPU Time", "Bytes Rx", "Bytes Tx"]);
             for domain in list {
-                table.add_row(row![domain.name, domain.state, domain.memory, domain.max_mem, domain.nr_virt_cpu, domain.cpu_time]);
+                let rx = if let Some(ifaces) = &domain.ifaces { ifaces.iter().fold(0, |acc, iface| acc + iface.rx_bytes).to_string() } else { "".to_string() };
+                let tx = if let Some(ifaces) = &domain.ifaces { ifaces.iter().fold(0, |acc, iface| acc + iface.tx_bytes).to_string() } else { "".to_string() };
+
+                table.add_row(row![
+                    domain.name, 
+                    domain.state,
+                    domain.memory,
+                    domain.max_mem,
+                    domain.nr_virt_cpu,
+                    domain.cpu_time,
+                    rx,
+                    tx,
+                ]);
             }
             table.printstd();
         },
